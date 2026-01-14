22b2205a3bcb425c5f76484c5a2bcea688ddd7eb
Evan Louie <evlouie@kite.local>
2026-01-13 20:01:52 -0800

    chore: update swift-argument-parser to 1.7.0

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         .library(name: "Upscaling", targets: ["Upscaling"])
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.7.0"),
         .package(url: "https://github.com/Finnvoor/SwiftTUI.git", from: "1.0.4"),
         .package(url: "https://github.com/apple/swift-testing.git", branch: "main")
     ],
