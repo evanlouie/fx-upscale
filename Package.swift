3d4de10a5cfa4ea806e0d5848c3c5ccdbbd1d555
evanlouie <2081587+evanlouie@users.noreply.github.com>
2026-04-17 22:10:27 -0700

    build(deps): bump swift-argument-parser to 1.7.1
    Update the lockfile to the latest patch release. No API changes required
    
    in our code.

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     .library(name: "Upscaling", targets: ["Upscaling"]),
   ],
   dependencies: [
-    .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.7.0"),
+    .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.7.1"),
   ],
   targets: [
     .executableTarget(
